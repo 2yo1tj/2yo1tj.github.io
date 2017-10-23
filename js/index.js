b13cb6d6791e37c7c1180ea9cd1e2d6d62aaac17
smtjong <samanthatjong@gmail.com>
2017-10-23 12:14:03 -0400

    updated index-test.html
    using generic sprites for now

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // create a reference to our element
-var element = document.querySelector('#kramer1');
+var element = document.querySelector('#sprite-test1');
 // create our Motio sprite
-var sprite = new Motio(element, {fps: 4, frames: 4});
+var sprite = new Motio(element, {fps: 4, frames: 2});
 // start animation
 sprite.play();
 
@@ -35,9 +35,9 @@ function animate() {
 animate();
 
 // create a reference to our element
-var element2 = document.querySelector('#kramer2');
+var element2 = document.querySelector('#sprite-test2');
 // create our Motio sprite
-var sprite2 = new Motio(element2, {fps: 8, frames: 4});
+var sprite2 = new Motio(element2, {fps: 8, frames: 2});
 // start animation
 sprite2.play();
 
@@ -69,3 +69,76 @@ function animate2() {
 
 // call our animation initially
 animate2();
+
+// create a reference to our element
+var element_test = document.querySelector('#sprite-test');
+// create our Motio sprite
+var sprite_test = new Motio(element_test, {fps: 4, frames: 2});
+// start animation
+sprite_test.play();
+
+// distance to be traveled
+var distance_test = window.innerWidth;
+
+// how many pixels per iteration to travel
+var speed_test = 2;
+
+// variable to hold our element's position
+var position_test = 0;
+
+function animate_test() {
+    // increment the position
+    position_test += speed_test;
+  
+    // apply the position to our element
+    element_test.style.left = position_test + "px";
+  
+    // check if element has gone offscreen 
+    // and move him back, if necessary
+    if (Math.abs(position_test) >= distance_test) {
+      position_test = -150;
+    }
+    
+    // tell the browser to update our animation
+    requestAnimationFrame(animate_test);
+}
+
+// call our animation initially
+animate_test();
+
+
+// create a reference to our element
+var element1 = document.querySelector('#sprite1');
+// create our Motio sprite
+var sprite1 = new Motio(element1, {fps: 4, frames: 2});
+// start animation
+sprite1.play();
+
+// distance to be traveled
+var distance1 = window.innerWidth;
+
+// how many pixels per iteration to travel
+var speed1 = 2;
+
+// variable to hold our element's position
+var position1 = 0;
+
+function animate1() {
+    // increment the position
+    position1 += speed1;
+  
+    // apply the position to our element
+    element1.style.left = position1 + "px";
+  
+    // check if element has gone offscreen 
+    // and move him back, if necessary
+    if (Math.abs(position1) >= distance1) {
+      position1 = -150;
+    }
+    
+    // tell the browser to update our animation
+    requestAnimationFrame(animate1);
+}
+
+// call our animation initially
+animate1();
